eed3aeac21ec949c4f4d95e3efdfedde44ca5294
Karan Dixit <97967796+dixitk941@users.noreply.github.com>
2024-04-13 04:49:57 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,7 +8,22 @@ const firebaseConfig = {
   storageBucket: "fir-auth-1132.appspot.com",
   messagingSenderId: "1029021654707",
   appId: "1:1029021654707:web:14aa4dee98449061701ac6",
-  measurementId: "G-MFP6M081T7",
+  measurementId: "G-MFP6M081T7",apiKey: "AIzaSyDLT5VGNrm5_Ks9gs8YlBvvS9rKrKjD2sY",
+
+  authDomain: "climate-check-360.firebaseapp.com",
+
+  databaseURL: "https://climate-check-360-default-rtdb.firebaseio.com",
+
+  projectId: "climate-check-360",
+
+  storageBucket: "climate-check-360.appspot.com",
+
+  messagingSenderId: "491332887364",
+
+  appId: "1:491332887364:web:6da35e591994f98f54ad11",
+
+  measurementId: "G-WL33DTYDFY",
+
 };
 
 const app = initializeApp(firebaseConfig);
