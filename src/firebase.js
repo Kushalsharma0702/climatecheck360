84ef4062de0167d1cbe6330b61f68bdeb130d930
Karan Dixit <97967796+dixitk941@users.noreply.github.com>
2024-04-13 05:18:18 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,26 +2,17 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-apiKey: "AIzaSyDLT5VGNrm5_Ks9gs8YlBvvS9rKrKjD2sY",
-
+  apiKey: "AIzaSyDLT5VGNrm5_Ks9gs8YlBvvS9rKrKjD2sY",
   authDomain: "climate-check-360.firebaseapp.com",
-
   databaseURL: "https://climate-check-360-default-rtdb.firebaseio.com",
-
   projectId: "climate-check-360",
-
   storageBucket: "climate-check-360.appspot.com",
-
   messagingSenderId: "491332887364",
-
   appId: "1:491332887364:web:6da35e591994f98f54ad11",
-
-  measurementId: "G-WL33DTYDFY",
-
+  measurementId: "G-WL33DTYDFY"
 };
 
 const app = initializeApp(firebaseConfig);
-
 const auth = getAuth();
 
 export { app, auth };
