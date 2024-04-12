fb8bfa1ca6ff0cba7db65953a01700bbe6042f7c
Karan Dixit <97967796+dixitk941@users.noreply.github.com>
2024-04-13 04:38:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar"; // Adjust the path as needed
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import Signup from "./components/Signup/Signup";
 import CurrentLocation from "./currentLocation";
+import { auth } from "./firebase";
 import "./App.css";
 
 function App() {
+  const [userName, setUserName] = useState("");
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUserName(user.displayName);
+      } else {
+        setUserName("");
+      }
+    });
+  }, []);
+
   return (
     <React.Fragment>
-    <Navbar />
+      <Navbar />
       <div className="container">
+        <Router>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/" element={<Home name={userName} />} />
+          </Routes>
+        </Router>
         <CurrentLocation />
       </div>
-     
     </React.Fragment>
   );
 }
